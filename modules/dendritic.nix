57249be5a2c567f8ec5e0a6f2863cdea4f43cd6e
Michael Zuercher <michzuerch@gmail.com>
2026-02-27 01:07:17 +0100

    add inputs.nix

--- modules/dendritic.nix
+++ modules/dendritic.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 {
   imports = [
     (inputs.flake-file.flakeModules.dendritic or { })
@@ -7,8 +7,8 @@
 
   # other inputs may be defined at a module using them.
   flake-file.inputs = {
-    den.url = "github:vic/den";
-    flake-file.url = "github:vic/flake-file";
+    den.url = lib.mkDefault "github:vic/den";
+    flake-file.url = lib.mkDefault "github:vic/flake-file";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
---
+++ modules/inputs.nix
@@ -0,0 +1,12 @@
+{ ... }:
+{
+  flake-file.inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    ucodenix.url = "github:e-tho/ucodenix";
+  };
+}
